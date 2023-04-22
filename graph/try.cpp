1e8aec75ecf8c9279aa3794ddfa70fefa66252b0
Swapnil Bankar <swapnilbankar1010@gmail.com>
2023-04-22 08:39:37 +0530

    graph

--- graph/try.cpp
+++ graph/try.cpp
@@ -55,51 +55,103 @@
 // }
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// const int N = 1e5+2;
+// bool vis[N];
+// vector<int>adj[N];
+
+
+// bool dfs(int vertex,int par){
+//     vis[vertex] = true;
+//     bool isLoopExist =  false;
+//     for(int childs: adj[vertex]){
+//         if(vis[childs] && childs==par) continue;
+//         if (vis[childs]) return true;
+//         isLoopExist |= dfs(childs,vertex);
+//     }
+// }
+
+
+
+// int main(){
+
+//     int n,m;
+//     cin>>n>>m;
+
+//     int x,y;
+
+//     for(int i=0;i<m;i++){
+
+//         cin>>x>>y;
+//         adj[x].push_back(y);
+//         adj[y].push_back(x);
+
+//     }
+//     bool isLoopExist=false;
+
+//     for(int i=1;i<=n;i++){
+//         if(vis[i]) continue;
+//         if(dfs(i,0)){
+//             isLoopExist = true;
+//             break;
+//         }
+
+//     }
+
+//     cout<<isLoopExist<<endl;
+
+// }
+
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
 const int N = 1e5+2;
 bool vis[N];
 vector<int>adj[N];
+int level[N];
 
-
-bool dfs(int vertex,int par){
-    vis[vertex] = true;
-    bool isLoopExist =  false;
-    for(int childs: adj[vertex]){
-        if(vis[childs] && childs==par) continue;
-        if (vis[childs]) return true;
-        isLoopExist |= dfs(childs,vertex);
+void bfs(int vertex){
+    queue<int>q;
+    q.push(vertex);
+    vis[vertex] = 1;
+    while (!q.empty())
+    {
+        int curr_v = q.front();
+        q.pop();
+        cout<<curr_v<<" ";
+        for(int child: adj[curr_v]){
+            if(!vis[child]){
+                q.push(child);
+                vis[child]=1;
+                level[child]=level[curr_v];
+            }
+        }
     }
+    
 }
 
 
 
 int main(){
 
-    int n,m;
-    cin>>n>>m;
+    int n;
+    cin>>n;
 
     int x,y;
 
-    for(int i=0;i<m;i++){
+    for(int i=0;i<n-1;i++){
 
         cin>>x>>y;
         adj[x].push_back(y);
         adj[y].push_back(x);
 
     }
-    bool isLoopExist=false;
-
-    for(int i=1;i<=n;i++){
-        if(vis[i]) continue;
-        if(dfs(i,0)){
-            isLoopExist = true;
-            break;
-        }
-
-    }
-
-    cout<<isLoopExist<<endl;
+    bfs(1);
 
 }
