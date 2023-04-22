d7ff37825b9c2a3b5362ef3560357d260fee1b6f
Swapnil Bankar <swapnilbankar1010@gmail.com>
2023-04-22 08:25:32 +0530

    graph

--- graph/DFS.cpp
+++ graph/DFS.cpp
@@ -1,3 +1,5 @@
+//dfs with connected component count
+
 // #include<bits/stdc++.h>
 // using namespace std;
 
@@ -70,7 +72,7 @@
 // }
 
 
-
+//dfs 
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -85,6 +87,7 @@ void dfa(int vertex){
     vis[vertex]= true;
 
     for(int child: adj[vertex]){
+        cout<<"par "<<vertex<<" child "<<child<<endl;
         if(vis[child]) continue;
 
         dfa(child);
@@ -108,19 +111,117 @@ int main(){
     }
 
     dfa(1);
-
-
-    // code for connected graph count
     
-    int count =0;
+}
+
+
+//dfs with connected component count with elements of connected component
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// const int N = 1e5+2;
+// bool vis[N];
+// vector<int>adj[N];
+// vector<vector<int>> cc;
+// vector<int>current_cc;
+
+// void dfs(int vertex){
+//     // cout<<vertex<<endl;
+//     vis[vertex] = true;
+//     current_cc.push_back(vertex);
+//     for(int childs: adj[vertex]){
+//         // cout<<"par "<<vertex<<" child "<<childs<<endl;
+//         if (vis[childs]) continue;
+//         dfs(childs);
+//     }
+// }
+
+
+
+// int main(){
+
+//     int n,m;
+//     cin>>n>>m;
+
+//     int x,y;
+
+//     for(int i=0;i<m;i++){
+
+//         cin>>x>>y;
+//         adj[x].push_back(y);
+//         adj[y].push_back(x);
+
+//     }
+
+//     for(int i=1;i<=n;i++){
+//         if(vis[i]) continue;
+//         current_cc.clear();
+//         dfs(i);
+//         cc.push_back(current_cc);
+//     }
+
+//     cout<<cc.size()<<endl;
+
+//     for(auto c_cc: cc){
+//         for(int vertex: c_cc)
+//         {
+//             cout<<vertex<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+
+//dfs cycle detection codde
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5+2;
+bool vis[N];
+vector<int>adj[N];
+
+
+bool dfs(int vertex,int par){
+    vis[vertex] = true;
+    bool isLoopExist =  false;
+    for(int childs: adj[vertex]){
+        if(vis[childs] && childs==par) continue;
+        if (vis[childs]) return true;
+        isLoopExist |= dfs(childs,vertex);
+    }
+}
+
+
+
+int main(){
+
+    int n,m;
+    cin>>n>>m;
+
+    int x,y;
+
+    for(int i=0;i<m;i++){
+
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+
+    }
+    bool isLoopExist=false;
 
     for(int i=1;i<=n;i++){
         if(vis[i]) continue;
-        dfa(i);
-        count++;
+        if(dfs(i,0)){
+            isLoopExist = true;
+            break;
+        }
+
     }
 
-    cout<<count<<endl;
+    cout<<isLoopExist<<endl;
 
-    
 }
--- graph/try.cpp
+++ graph/try.cpp
@@ -1,59 +1,105 @@
-#include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// const int N = 1e5+2;
+// bool vis[N];
+// vector<int>adj[N];
+// vector<vector<int>> cc;
+// vector<int>current_cc;
+
+// void dfs(int vertex){
+//     // cout<<vertex<<endl;
+//     vis[vertex] = true;
+//     current_cc.push_back(vertex);
+//     for(int childs: adj[vertex]){
+//         // cout<<"par "<<vertex<<" child "<<childs<<endl;
+//         if (vis[childs]) continue;
+//         dfs(childs);
+//     }
+// }
+
+
+
+// int main(){
+
+//     int n,m;
+//     cin>>n>>m;
+
+//     int x,y;
+
+//     for(int i=0;i<m;i++){
+
+//         cin>>x>>y;
+//         adj[x].push_back(y);
+//         adj[y].push_back(x);
+
+//     }
+
+//     for(int i=1;i<=n;i++){
+//         if(vis[i]) continue;
+//         current_cc.clear();
+//         dfs(i);
+//         cc.push_back(current_cc);
+//     }
+
+//     cout<<cc.size()<<endl;
+
+//     for(auto c_cc: cc){
+//         for(int vertex: c_cc)
+//         {
+//             cout<<vertex<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
 const int N = 1e5+2;
-int vis[N];
-vector<int> adj[N];
-int level[N];
+bool vis[N];
+vector<int>adj[N];
 
 
-
-void bfs(int source){
-
-    queue<int>q;
-    q.push(source);
-    vis[source] = 1;
-
-    while (!q.empty())
-    {
-        int cur_v = q.front();
-        q.pop();
-        cout<< cur_v << " ";
-        for(int child: adj[cur_v]){
-            if(!vis[child]){
-                q.push(child);
-                vis[child]= 1;
-                level[child] = level[cur_v] + 1;
-            }
-
-        }
+bool dfs(int vertex,int par){
+    vis[vertex] = true;
+    bool isLoopExist =  false;
+    for(int childs: adj[vertex]){
+        if(vis[childs] && childs==par) continue;
+        if (vis[childs]) return true;
+        isLoopExist |= dfs(childs,vertex);
     }
-    cout<<endl;
 }
+
+
 
 int main(){
 
-    int n;
-    cin>>n;
+    int n,m;
+    cin>>n>>m;
 
-    for(int i = 0 ; i<n-1 ; ++i){
+    int x,y;
 
-        int x,y;
+    for(int i=0;i<m;i++){
+
         cin>>x>>y;
         adj[x].push_back(y);
         adj[y].push_back(x);
+
     }
-
-    bfs(1);
-
-    bfs(1);
+    bool isLoopExist=false;
 
     for(int i=1;i<=n;i++){
-        cout<< i <<": "<<level[i]<<endl;
+        if(vis[i]) continue;
+        if(dfs(i,0)){
+            isLoopExist = true;
+            break;
+        }
+
     }
 
-
-    return 0;
+    cout<<isLoopExist<<endl;
 
 }
