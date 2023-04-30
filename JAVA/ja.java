198a93602aea3e0d35e4f3fc26baac220f7a432e
Swapnil Bankar <swapnilbankar1010@gmail.com>
2023-04-30 22:03:12 +0530

    java

--- JAVA/ja.java
+++ JAVA/ja.java
@@ -1,9 +1,9 @@
 import java.util.*;
 class ja{
     public static void main(String args[]) {
-    //     int a;
-    //     Scanner sc = new Scanner(System.in);
-    //     a= sc.nextInt();
-    //     System.out.println(a);
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a= sc.nextInt();
+        System.out.println(a);
     }
 }
