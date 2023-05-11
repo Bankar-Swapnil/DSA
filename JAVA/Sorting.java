adfbf6e97701255e93d91b4655f79d73649ac6f0
Swapnil Bankar <swapnilbankar1010@gmail.com>
2023-05-11 20:07:22 +0530

    java

--- JAVA/Sorting.java
+++ JAVA/Sorting.java
@@ -1,18 +1,18 @@
 public class Sorting {
 
 
-    public  static void selectionSort(int arr[]){
+    // public  static void selectionSort(int arr[]){
 
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
-                    int temp = arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
-                }
-            }
-        }
-    }
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=i+1;j<arr.length;j++){
+    //             if(arr[i]>arr[j]){
+    //                 int temp = arr[i];
+    //                 arr[i]=arr[j];
+    //                 arr[j]=temp;
+    //             }
+    //         }
+    //     }
+    // }
 
     public static void bubbleSort(int arr[]){
 
@@ -82,8 +82,8 @@ public class Sorting {
 			quickSort(arr, low, pi-1);
 			quickSort(arr, pi+1, high);
 		}
-        
 	}
+
 
 
     public static void merge(int arr[],int l,int mid,int r){
@@ -128,7 +128,7 @@ public class Sorting {
                 k++;
         }
 
-}
+    }
 
 
 
