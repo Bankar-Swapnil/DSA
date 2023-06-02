1fa7fe63a5a04567d48d3a31e2e664886629410f
Swapnil Bankar <swapnilbankar1010@gmail.com>
2023-06-02 08:50:13 +0530

    new\

--- JAVA/sorting/Sorting.java
+++ JAVA/sorting/Sorting.java
@@ -247,16 +247,16 @@ public class Sorting {
         }
     }
 
-    // public static void bubbleSort2(int arr[]){
-    //     for(int i=0;i<arr.length-1;i++){
-    //         for(int j=0;j<arr.length-i-1;j++){
-    //             if(arr[j]>arr[j+1]){
-    //                 int temp =arr[j];
-    //                 arr[j]=arr[j+1];
-    //                 arr[j+1]=temp;
-    //             }
-    //         }
-    //     }
+    public static void bubbleSort2(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp =arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
     }
 
 
