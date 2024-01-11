3a02ca40e3b8ec36d49986e154e581ec59d56faa
Swapnil Bankar <swapnilbankar1010@gmail.com>
2024-01-11 14:14:12 +0530

    new

--- _Interview_Basic/Patterns/patterns.java
+++ _Interview_Basic/Patterns/patterns.java
@@ -39,45 +39,45 @@ public class patterns {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=n;j++ ){
-        //         if(i==1 || i==n || j==1 || j==n ){
-        //             System.out.print("*");
-        //         }else{
-        //             System.out.print(" ");
-        //         }
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++ ){
+                if(i==1 || i==n || j==1 || j==n ){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
 
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=n-i;j++){
-        //         System.out.print(" ");
-        //     }
-        //     for(int k=1;k<=i;k++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
-        // for(int i=n;i>0;i--){
-        //     for(int j=1;j<=i;j++){
-        //         System.out.print(j+" ");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
 
-        // int k=1;
+        int k=1;
 
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=i;j++){
-        //         System.out.print(k+" ");
-        //         k++;
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
 
 
         
