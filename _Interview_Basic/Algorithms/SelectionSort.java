1f88b95b09b610f821edf6ee66d32523efb3c074
Swapnil Bankar <swapnilbankar1010@gmail.com>
2024-01-16 19:05:00 +0530

    new

---
+++ _Interview_Basic/Algorithms/SelectionSort.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+    
+    public static void selection(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void display(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,7,2,1,4,3};
+        selection(arr);
+        display(arr);
+    }
+}
---
+++ _Interview_Basic/Algorithms/quickSort.java
@@ -0,0 +1,45 @@
+public class quickSort {
+    public static int partition(int arr[],int l,int h){
+
+        int pivot =arr[h];
+        int i=l-1;
+
+        for(int j=l;j<h;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+
+        int temp = arr[i+1];
+        arr[i+1]=arr[h];
+        arr[h]=temp;
+
+        return i+1;
+
+    }
+
+
+    public static void quickSort2(int arr[],int l,int h){
+        if(l<h){
+            int partitionIndex=partition(arr, l, h);
+            quickSort2(arr, l, partitionIndex-1);
+            quickSort2(arr,partitionIndex+1,h);
+
+        }
+    }
+
+    public static void display(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,7,2,1,4,3};
+        quickSort2(arr,0,arr.length-1);
+        display(arr);
+    }
+}
