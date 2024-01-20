11cef31ddf047a86cea2811a193be839968d0128
Swapnil Bankar <swapnilbankar1010@gmail.com>
2024-01-20 11:36:59 +0530

    new

--- _Interview_Basic/Tree/buildTree.java
+++ _Interview_Basic/Tree/buildTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class buildTree{
     static class node {
         int data;
@@ -54,14 +57,56 @@ class buildTree{
             System.out.print(root.data+" ");
             inOrder(root.right);
         }
+        
+        public static void levelOrder(node root ){
+            if(root==null){
+                return;
+            }
+            Queue<node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                node curr = q.remove();
+                if(curr==null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(curr.data+" ");
+                    if(curr.left!=null){
+                        q.add(curr.left);
+                    }
+                    if (curr.right!=null) {
+                        q.add(curr.right);
+                    }
+                }
+            }
+        }
+
+        public static int hightTree(node root){
+            if(root==null){
+                return 0;
+            }
+
+            return Math.max(hightTree(root.left),hightTree(root.right))+1;
+        }
     }
 
     public static void main(String[] args) {
         tree a = new tree();
         int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         node root = a.buildee( nodes);
-        System.out.println(root.data);
-        
-        a.preOrder(root);
+
+        // System.out.println(root.data);
+        // a.preOrder(root);
+
+        // a.levelOrder(root);
+
+        System.out.println(a.hightTree(root));
+
     }
 }
