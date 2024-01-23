635c27eb92551db507f749551ec59f120ea6ffe7
Swapnil Bankar <swapnilbankar1010@gmail.com>
2024-01-23 08:16:03 +0530

    new

--- _Interview_Basic/String/AllSubsets.java
+++ _Interview_Basic/String/AllSubsets.java
@@ -17,9 +17,9 @@ public class AllSubsets {
         }  
   
         // This loop prints all the subsets formed from the string  
-        // System.out.println("All subsets for the given string are: ");  
-        // for (int i = 0; i < arr.length; i++) {  
-        //     System.out.println(arr[i]);  
-        // }  
+        System.out.println("All subsets for the given string are: ");  
+        for (int i = 0; i < arr.length; i++) {  
+            System.out.println(arr[i]);  
+        }  
     }  
 }
